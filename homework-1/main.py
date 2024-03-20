89fc0c1d72e750f1fa1d1329a73e763e01e80fba
Mihail <vislapyz@mail.ru>
2024-03-20 17:43:06 +0300

    Создание таблицы

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,9 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="123qweQWE"
+)
